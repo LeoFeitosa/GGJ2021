e6842ee09a6da04ddfce8d5d1287914dc40faab7
Leo Feitosa <ffeitosa.leonardo@gmail.com>
2021-01-28 16:38:36 -0300

    add animation jump and fall

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    Animator _animator;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void SetGrounded(bool input)
+    {
+        _animator.SetBool("isGrounded", input);
+    }
+    public void SetSpeedY(float input)
+    {
+        _animator.SetFloat("speedY", input);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,27 +5,59 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D _rigidbody2D;
+    PlayerAnimator _playerAnimator;
 
     [SerializeField]
     float jumpForce = 400;
 
+    [SerializeField]
+    LayerMask layerGround;
+
+    [SerializeField]
+    Transform groundCheck;
+
+    [SerializeField]
+    float groundCheckRadius = 0.02f;
+
+    [SerializeField]
+    bool isGround;
+
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _playerAnimator = GetComponentInChildren<PlayerAnimator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGround)
         {
             Jump();
         }
+
+        RunAnimations();
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGround();
     }
 
     void Jump()
     {
         _rigidbody2D.velocity = Vector2.zero;
         _rigidbody2D.AddForce(Vector2.up * jumpForce);
+    }
+
+    void CheckGround()
+    {
+        isGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, layerGround);
+    }
+
+    void RunAnimations()
+    {
+        _playerAnimator.SetGrounded(isGround);
+        _playerAnimator.SetSpeedY(_rigidbody2D.velocity.y);
     }
 }
