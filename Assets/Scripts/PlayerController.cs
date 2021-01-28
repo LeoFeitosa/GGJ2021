a921d2b838e18781f931b5268dba78cfc47f8e01
Leo Feitosa <ffeitosa.leonardo@gmail.com>
2021-01-28 15:34:05 -0300

    config initial

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D _rigidbody2D;
+
+    [SerializeField]
+    float jumpForce = 400;
+
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        _rigidbody2D.velocity = Vector2.zero;
+        _rigidbody2D.AddForce(Vector2.up * jumpForce);
+    }
+}
