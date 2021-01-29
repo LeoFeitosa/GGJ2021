c2c3c5b4c23fdd6550e4cb7fb640e993a43616bd
Hevelyn Hespanhol <hevelyn.hespanhol@hotmail.com>
2021-01-29 18:17:23 -0300

    Implementação da função parallax

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 1f;
+
+    [SerializeField]
+    float offset;
+    private Vector2 startPosition;
+    private float newXposition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newXposition = Mathf.Repeat(Time.time * -moveSpeed, offset);
+        transform.position = startPosition + Vector2.right * newXposition;
+    }
+}
