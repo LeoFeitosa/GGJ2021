c291dd3b999adb2f420e30a960775abda34cc3d6
Leo Feitosa <ffeitosa.leonardo@gmail.com>
2021-01-29 22:58:06 -0300

    move obstacles

---
+++ Assets/Scripts/Obstacles.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+    PlayerController _playerController;
+    SpriteRenderer _spriteRenderer;
+
+    //velocidade do movimento
+    [SerializeField]
+    float speed;
+
+    Vector3 direction = Vector3.zero;
+
+    void Start()
+    {
+        _playerController = FindObjectOfType(typeof(PlayerController)) as PlayerController;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        //movimenta o obstaculo da direita para esquerda
+        Flip();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //destroi a peca que colidiu
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Flip()
+    {
+        // direcao da obstaculo
+        if (_playerController.isLookLeft)
+        {
+            direction = Vector3.right;
+            transform.position += direction * speed * Time.deltaTime;
+            _spriteRenderer.flipX = false;
+        }
+        if(!_playerController.isLookLeft)
+        {
+            direction = Vector3.right;
+            transform.position -= direction * speed * Time.deltaTime;
+            _spriteRenderer.flipX = true;
+        }
+    }
+
+    //se sair da tela é destruido
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/SpawObstacles.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawObstacles : MonoBehaviour
+{
+    PlayerController _playerController;
+
+    [SerializeField]
+    GameObject obstacle1;
+
+    [SerializeField]
+    GameObject obstacle2;
+
+    [SerializeField]
+    GameObject obstacle3;
+
+    GameObject obstacleCurrent;
+
+    [SerializeField]
+    float height;
+
+    [SerializeField]
+    float maxTime = 2f;
+
+    [SerializeField]
+    float destroyTime = 6f;
+
+    float timer = 0f;
+
+    void Start()
+    {
+        _playerController = FindObjectOfType(typeof(PlayerController)) as PlayerController;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("GetObstacle", 2);
+    }
+
+    void GetObstacle()
+    {
+        if (timer > maxTime)
+        {
+            int rand = Random.Range(0, 4);
+            if (rand == 1)
+            {
+                obstacleCurrent = Instantiate(obstacle1);
+            }
+            if (rand == 2)
+            {
+                obstacleCurrent = Instantiate(obstacle2);
+            }
+            else
+            {
+                obstacleCurrent = Instantiate(obstacle3);
+            }
+
+            obstacleCurrent.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+
+            //verifica o lado do player para definir o lado da instancia
+            if (_playerController.isLookLeft)
+            {
+                obstacleCurrent.transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+                obstacleCurrent.transform.position = new Vector3(-obstacleCurrent.transform.position.x, obstacleCurrent.transform.position.y, obstacleCurrent.transform.position.z);
+            }
+
+            //destroi o objeto depois de um tempo
+            Destroy(obstacleCurrent, destroyTime);
+
+            //zera a contagem
+            timer = 0;
+        }
+
+        //soma a contagem
+        timer += Time.deltaTime;
+    }
+}
