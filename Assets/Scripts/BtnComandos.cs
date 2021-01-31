693e2a66e9ff40caa205a7df887566b6039718be
Hevelyn Hespanhol <hevelyn.hespanhol@hotmail.com>
2021-01-31 03:39:37 -0300

    Implementação do Menu

--- Assets/Scripts/BtnComandos.cs
+++ Assets/Scripts/BtnComandos.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class BtnComandos : MonoBehaviour
-{    
+{   
+
+    public GameObject painel1, painel2;
+
+    //Método que vai para a outra cena 
     public void IrParaCena(string nomeCena)
     {
         SceneManager.LoadScene(nomeCena);
+    }
+
+    //Método que fecha o jogo (Só funciona em PC e Mobile)
+    public void Sair()
+    {
+        Application.Quit();
+    }
+
+    public void Creditos(bool onOff)
+    {
+        painel1.SetActive(!onOff);
+        painel2.SetActive(onOff);
     }
 
 }
