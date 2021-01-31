b5fb13c83279bbc00044b436d01800ae4d5187af
Hevelyn Hespanhol <hevelyn.hespanhol@hotmail.com>
2021-01-30 23:20:54 -0300

    Arrumando alguns bugs da Scena de Gameplay e adicionando a Scena de Menu

---
+++ Assets/Scripts/BtnComandos.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BtnComandos : MonoBehaviour
+{    
+    public void IrParaCena(string nomeCena)
+    {
+        SceneManager.LoadScene(nomeCena);
+    }
+
+}
